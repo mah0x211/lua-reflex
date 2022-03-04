02125e955a99afe0a7623f086983a6768377627f
Masatoshi Fukunaga <mah0x211@gmail.com>
2022-03-04 15:13:22 +0900

    Change the return value of the exec function

--- test/exec_test.lua
+++ test/exec_test.lua
@@ -4,11 +4,48 @@ local testcase = require('testcase')
 local exec = require('reflex.exec')
 
 function testcase.exec()
+    -- test that return true
+    local ok, res = exec('lua', {
+        '-e',
+        [[io.stdout:write('hello')]],
+    })
+    assert(ok, res)
+    assert.equal(res, 'hello')
+
+    -- test that return error with invalid syntax
+    ok, res = exec('lua', {
+        '-e',
+        [[print(']],
+    })
+    assert.is_false(ok)
+    assert.match(res, 'unfinished string')
+
+    -- test that return error with invalid code
+    ok, res = exec('lua', {
+        '-e',
+        [[print(foo + 2)]],
+    })
+    assert.is_false(ok)
+    assert.match(res, 'attempt to perform arithmetic')
+
+    -- test that throws an error if invalid argument
+    local err = assert.throws(exec, 'lua', {
+        '-v',
+    }, nil, 1)
+    assert.match(err, 'stdout must be function')
+
+    err = assert.throws(exec, 'lua', {
+        '-v',
+    }, nil, nil, 1)
+    assert.match(err, 'stderr must be function')
+end
+
+function testcase.exec_with_stdio()
     local outlines = {}
     local errlines = {}
 
     -- test that return true
-    local ok, err = exec('lua', {
+    local ok, res = exec('lua', {
         '-e',
         [[io.stdout:write('hello'); io.stderr:write('world')]],
     }, nil, function(...)
@@ -18,7 +55,8 @@ function testcase.exec()
     end, function(line)
         errlines[#errlines + 1] = line
     end)
-    assert(ok, err)
+    assert(ok, res)
+    assert.is_nil(res)
     assert.equal(outlines, {
         [[lua -e io.stdout:write('hello'); io.stderr:write('world')]],
         'hello',
@@ -27,30 +65,14 @@ function testcase.exec()
         'world',
     })
 
-    -- test that return error with invalid syntax
-    ok, err = exec('lua', {
-        '-e',
-        [[print(']],
-    })
-    assert.is_false(ok)
-    assert.match(err, 'unfinished string')
-
     -- test that return error with invalid code
-    ok, err = exec('lua', {
+    errlines = {}
+    ok, res = exec('lua', {
         '-e',
         [[print(foo + 2)]],
-    })
+    }, nil, nil, function(line)
+        errlines[#errlines + 1] = line
+    end)
     assert.is_false(ok)
-    assert.match(err, 'attempt to perform arithmetic')
-
-    -- test that throws an error if invalid argument
-    err = assert.throws(exec, 'lua', {
-        '-v',
-    }, nil, 1)
-    assert.match(err, 'stdout must be function')
-
-    err = assert.throws(exec, 'lua', {
-        '-v',
-    }, nil, nil, 1)
-    assert.match(err, 'stderr must be function')
+    assert.equal(res, concat(errlines, '\n'))
 end
