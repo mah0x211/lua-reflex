9f1c6a19a2293ca3e41994854e7745c64da3ab3c
Masatoshi Fukunaga <mah0x211@gmail.com>
2022-03-04 15:23:37 +0900

    Fix bug that static filepaths ware not included in the route list

--- test/router_test.lua
+++ test/router_test.lua
@@ -98,6 +98,9 @@ function testcase.new()
                     },
                 },
             },
+            ['/signout'] = {
+                handlers = {},
+            },
             ['/:user'] = {
                 handlers = {
                     {
