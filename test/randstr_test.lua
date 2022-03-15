20111203357b6a52b79457b703e33919c73082cc
Masatoshi Fukunaga <mah0x211@gmail.com>
2022-03-15 20:12:56 +0900

    Add an encode argument to the randstr function

--- test/randstr_test.lua
+++ test/randstr_test.lua
@@ -4,11 +4,18 @@ local randstr = require('reflex.randstr')
 
 function testcase.randstr()
     -- test that returns a specified length of random string
-    local tbl = {}
     for i = 1, 1000, 50 do
         local s = assert(randstr(i))
         assert.equal(#s, i)
-        assert.is_nil(tbl[s])
-        tbl[s] = 1
+        assert.match(s, '^%d+$', false)
+    end
+end
+
+function testcase.randstr_with_encode()
+    -- test that returns a specified length of random string
+    for i = 1, 1000, 50 do
+        local s = assert(randstr(i, true))
+        assert.equal(#s, i)
+        assert.match(s, '^[a-zA-Z0-9_%-]+$', false)
     end
 end
