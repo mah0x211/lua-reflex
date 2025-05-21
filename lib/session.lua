38dbd5eac187f8d405fbbdbec89e49e7c0345a10
Masatoshi Fukunaga <mah0x211@users.noreply.github.com>
2025-05-21 13:29:18 +0900

    Use session module

--- lib/session.lua
+++ lib/session.lua
@@ -19,30 +19,18 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 --
-local find = string.find
-local getmetatable = debug.getmetatable
-local errorf = require('error').format
-local isa = require('isa')
-local is_finite = isa.finite
-local is_boolean = isa.boolean
-local is_string = isa.string
-local is_table = isa.table
-local is_function = isa.Function
-local new_cookie = require('cookie').new
-local bake_cookie = require('cookie').bake
-local uuid4str = require('ossp-uuid').gen4str
-local fatalf = require('error').fatalf
+local new_session_manager = require('session').new
 
 --- session-cookie name
 local DEFAULT_NAME = 'sid'
-local NAME = DEFAULT_NAME
-
 local DEFAULT_PATH_ATTR = '/'
 local DEFAULT_MAXAGE_ATTR = 60 * 30
 local DEFAULT_SECURE_ATTR = true
 local DEFAULT_HTTPONLY_ATTR = true
 local DEFAULT_SAMESITE_ATTR = 'lax'
-local ATTR = {
+local DEFAULT_COOKIE_CONFIG = {
+    name = DEFAULT_NAME,
+    domain = '',
     path = DEFAULT_PATH_ATTR,
     maxage = DEFAULT_MAXAGE_ATTR,
     secure = DEFAULT_SECURE_ATTR,
@@ -50,299 +38,68 @@ local ATTR = {
     samesite = DEFAULT_SAMESITE_ATTR,
 }
 
---- bake_attributes
---- @param newattr table
---- @param attr table
---- @return table
-local function bake_attributes(newattr, attr)
-    for _, field in ipairs({
-        'domain',
-        'path',
-        'maxage',
-        'secure',
-        'httponly',
-        'samesite',
-    }) do
-        if newattr[field] == nil then
-            local v = attr[field]
-            if v == nil then
-                v = ATTR[field]
-            end
-            newattr[field] = v
-        end
-    end
+local Manager = new_session_manager({
+    cookie = DEFAULT_COOKIE_CONFIG,
+})
 
-    return newattr
-end
-
--- use cache.inmem module as default session store
-local Store = require('cache.inmem').new(ATTR.maxage)
+--- @class session.store
+--- @field get fun(self, key: string, ttl: integer?):(ok: boolean, err: any, timeout: boolean?)
+--- @field set fun(self, key: string, val: any, ttl: integer?):(ok: boolean, err: any)
+--- @field delete fun(self, key: string):(ok: boolean, err: any)
+--- @field rename fun(self, key: string, newkey: string):(ok: boolean, err: any)
+--- @field keys fun(self, callback: fun(key: string):(ok: boolean, err: any), ...):(ok: boolean, err: any)
+--- @field evict fun(self, callback: fun(key: string):(ok: boolean, err: any), n: integer?, ...):(n: integer, err: any)
 
 --- set_store
---- @param store cache
+--- @param store session.store
 local function set_store(store)
-    local t = store
-    if not is_table(store) then
-        local mt = getmetatable(store)
-        t = is_table(mt) and is_table(mt.__index) and mt.__index or {}
-    end
+    assert(store ~= nil, 'store must not be nil')
 
-    if not is_function(t.set) or not is_function(t.get) or
-        not is_function(t.delete) then
-        fatalf(2, 'store must have %q, %q and %q methods', 'set', 'get',
-               'delete')
-    end
-
-    Store = store
+    -- create new session.Manager with specified store
+    local cookie = Manager.cookie
+    Manager = new_session_manager({
+        store = store,
+    })
+    Manager.cookie = cookie
 end
 
---- get_name
---- @return string name
-local function get_name()
-    return NAME
+--- set cookie configuration
+--- @param cfg table?
+local function set_cookie_config(cfg)
+    assert(cfg == nil or type(cfg) == 'table', 'cfg must be nil or table')
+    Manager.cookie:set_config(cfg or DEFAULT_COOKIE_CONFIG)
 end
 
---- set_name
---- @param name string
-local function set_name(name)
-    if name == nil then
-        name = DEFAULT_NAME
-    end
-    new_cookie(name, ATTR)
-    NAME = name
+--- get cookie configuration
+--- @return table
+local function get_cookie_config()
+    return Manager.cookie:get_config()
 end
 
---- get_attr
---- @return table attr
-local function get_attr()
-    local attr = {}
-    for k, v in pairs(ATTR) do
-        attr[k] = v
-    end
-    return attr
+--- @class session.Session
+
+--- create new session object
+--- @return session.Session
+local function new()
+    return Manager:create()
 end
 
---- set_attr
---- @param attr table
-local function set_attr(attr)
-    if attr == nil then
-        attr = {
-            path = DEFAULT_PATH_ATTR,
-            maxage = DEFAULT_MAXAGE_ATTR,
-            secure = DEFAULT_SECURE_ATTR,
-            httponly = DEFAULT_HTTPONLY_ATTR,
-            samesite = DEFAULT_SAMESITE_ATTR,
-        }
-    end
-
-    new_cookie(NAME, attr)
-    if attr.maxage and (not is_finite(attr.maxage) or attr.maxage < 1) then
-        fatalf('attr.maxage must be integer greater than 0')
-    end
-
-    ATTR.domain = attr.domain
-    for _, k in ipairs({
-        'maxage',
-        'path',
-        'secure',
-        'httponly',
-        'samesite',
-    }) do
-        local v = attr[k]
-        if v ~= nil then
-            ATTR[k] = v
-        end
-    end
-end
-
---- restore
---- @param id string
---- @return table? value
+--- restore session from cookie
+--- @param sid string
+--- @return session.Session? s
 --- @return any err
-local function restore(id)
-    if not is_string(id) then
-        fatalf('id must be string')
-    end
-
-    local data, err = Store:get(id, ATTR.maxage)
-    if err then
-        return nil, errorf('failed to Store:get()', err)
-    end
-
-    return data
-end
-
---- @class reflex.session
---- @field id string
---- @field value table<string, any>
-local Session = {}
-
---- init
---- @param cookies table<string, string>
---- @param restore_only boolean
---- @return reflex.session? ses
---- @return any err
-function Session:init(cookies, restore_only)
-    if cookies ~= nil and not is_table(cookies) then
-        fatalf('cookies must be table')
-    elseif restore_only ~= nil and not is_boolean(restore_only) then
-        fatalf('restore_only must be boolean')
-    end
-
-    if cookies then
-        local id = cookies[get_name()]
-        if id ~= nil then
-            local value, err = restore(id)
-            if err then
-                return nil, errorf('failed to restore()', err)
-            elseif value then
-                self.id = id
-                self.value = value
-                return self
-            end
-        end
-    end
-
-    if restore_only then
-        return nil
-    end
-
-    local newid, err = uuid4str()
-    if not newid then
-        return nil, errorf('failed to uuid4str()', err)
-    end
-
-    self.id = newid
-    self.value = {}
-    return self
-end
-
---- restore
---- @param id string
---- @return boolean ok
---- @return any err
-function Session:restore(id)
-    local value, err = restore(id)
-    if err then
-        return false, errorf('failed to restore()', err)
-    elseif not value then
-        return false
-    end
-
-    self.id = id
-    self.value = value
-    return true
-end
-
---- set
---- @param key string
---- @param val any
-function Session:set(key, val)
-    if not is_string(key) or find(key, '^%s+$') then
-        fatalf('key must be non-empty string')
-    end
-    self.value[key] = val
-end
-
---- delete
---- @param key string
---- @return any val
-function Session:delete(key)
-    if not is_string(key) then
-        fatalf('key must be string')
-    end
-
-    local val = self.value[key]
-    if val then
-        self.value[key] = nil
-    end
-
-    return val
-end
-
---- get
---- @param key string
---- @return any val
-function Session:get(key)
-    if not is_string(key) then
-        fatalf('key must be string')
-    end
-    return self.value[key]
-end
-
---- consume
---- @param key string
---- @return any val
-function Session:consume(key)
-    if not is_string(key) then
-        fatalf('key must be string')
-    end
-
-    local val = self.value[key]
-    if val then
-        -- delete value after get it
-        self.value[key] = nil
-    end
-
-    return val
-end
-
---- save
---- @param attr table?
---- @return string? cookie
---- @return any err
-function Session:save(attr)
-    if attr == nil then
-        attr = {}
-    elseif not is_table(attr) then
-        fatalf('attr must be table')
-    end
-
-    local ok, err = Store:set(self.id, self.value, ATTR.maxage)
-    if not ok then
-        return nil, errorf('failed to Store:set()', err)
-    end
-
-    return bake_cookie(NAME, self.id, bake_attributes({}, attr))
-end
-
---- destroy
---- @param attr table?
---- @return string? void_cookie
---- @return any err
-function Session:destroy(attr)
-    if attr == nil then
-        attr = {}
-    elseif not is_table(attr) then
-        fatalf('attr must be table')
-    end
-
-    local id, err = uuid4str()
-    if not id then
-        return nil, errorf('failed to uuid4str()', err)
-    end
-
-    local ok
-    ok, err = Store:delete(self.id)
-    if not ok then
-        return nil, errorf('failed to Store:delete()', err)
-    end
-
-    -- clear
-    self.id = id
-    self.value = {}
-    return bake_cookie(NAME, 'void', bake_attributes({
-        maxage = -60,
-    }, attr))
+--- @return boolean? timeout
+local function restore(sid)
+    assert(type(sid) == 'string', 'sid must be string')
+    return Manager:fetch(sid)
 end
 
 return {
-    new = require('metamodule').new(Session),
     set_store = set_store,
-    get_name = get_name,
-    set_name = set_name,
-    get_attr = get_attr,
-    set_attr = set_attr,
+    get_cookie_config = get_cookie_config,
+    set_cookie_config = set_cookie_config,
+    restore = restore,
+    new = new,
     -- export default config
     DEFAULT_NAME = DEFAULT_NAME,
     DEFAULT_PATH_ATTR = DEFAULT_PATH_ATTR,
